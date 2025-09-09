58a1110ed70ba6d4865d1ac4ec0abe16d402dff6
Nicole Ordoñez <NicoleMendoza1080@outlook.com>
2025-09-09 02:30:02 -0300

    Agregar archivos de proyecto.

---
+++ AuthApi/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using AuthApi.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+        private readonly IUsuarioRepository _usuarioRepository;
+
+        public UserController(IUsuarioRepository pUsuarioRepository)
+        {
+            _usuarioRepository = pUsuarioRepository;
+        }
+
+        [HttpGet("usuarios")]
+        [Authorize] // Solo usuarios autenticados
+        public async Task<IActionResult> GetUsuarios()
+        {
+            var usuarios = await _usuarioRepository.GetAllUsuariosAsync();
+            return Ok(usuarios);
+        }
+    }
+}
---
+++ AuthApi/Repositorios/AppContext.cs
@@ -0,0 +1,37 @@
+﻿using AuthApi.Entidades;
+
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+namespace AuthApi.Repositorios
+
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+             : base(options)
+        {
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Rol> Roles {  get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //Email único
+            modelBuilder.Entity<Usuario>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+
+            // Relación 1n Rol -> N Usuarios
+            modelBuilder.Entity<Usuario>()
+                .HasOne(u => u.Rol)
+                .WithMany(r => r.Usuarios)
+                .HasForeignKey(u => u.RolId);
+
+        }
+
+    }
+}
